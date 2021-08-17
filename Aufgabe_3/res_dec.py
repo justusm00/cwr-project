97a2ff1dccca79fdb0fa2b916c02351dc192d030
justus.multhaup <justus.multhaup@stud.uni-goettingen.de>
2021-08-17 11:26:50 +0200

    A_0 changed to \gamma

--- Aufgabe_3/res_dec.py
+++ Aufgabe_3/res_dec.py
@@ -15,7 +15,7 @@ xe = np.loadtxt("res_euler_dec.dat", usecols = 1) ##euler
 plt.figure(dpi=400)
 plt.grid()
 plt.xlabel("driving frequency $\omega$ in Hz")
-plt.ylabel("Amplitude $A/A_0$")
+plt.ylabel("Amplitude $A/\gamma$")
 plt.title("resonance curve for decreasing $\omega$")
 plt.plot(omega,xe, lw = 0.5,label = "Euler")
 plt.plot(omega,x2, lw = 0.5,label = "RK2")
