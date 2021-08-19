b646ae2e1f62d7f55fe3b3f38fe6b49f4253c5b4
justus.multhaup <justus.multhaup@stud.uni-goettingen.de>
2021-08-19 10:56:21 +0200

    plots merged into one file

---
+++ Aufgabe_3/plotA3.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+omega = np.loadtxt("res_euler_inc.dat", usecols = 0)
+x2 = np.loadtxt("res_rk2_inc.dat", usecols = 1) ##rk2
+x4 = np.loadtxt("res_rk4_inc.dat", usecols = 1) ##rk4
+xe = np.loadtxt("res_euler_inc.dat", usecols = 1) ##euler
+
+
+
+
+
+plt.figure(1)
+plt.grid()
+plt.xlabel("$\omega$")
+plt.ylabel("$x_\mathrm{max}$")
+plt.title("Resonance curve for increasing $\omega$")
+plt.plot(omega,xe,lw = 0.5, label = "Euler")
+plt.plot(omega,x2, lw = 0.5, label = "RK2")
+plt.plot(omega,x4, lw = 0.5, label = "RK4")
+
+plt.legend(loc = "upper right")
+plt.savefig("rescurve_inc.pdf",dpi = 400)
+plt.show()
+
+
+
+omega = np.loadtxt("res_euler_dec.dat", usecols = 0)
+x2 = np.loadtxt("res_rk2_dec.dat", usecols = 1) ##rk2
+x4 = np.loadtxt("res_rk4_dec.dat", usecols = 1) ##rk4
+xe = np.loadtxt("res_euler_dec.dat", usecols = 1) ##euler
+
+
+
+
+plt.figure(2)
+plt.grid()
+plt.xlabel("$\omega$")
+plt.ylabel("$x_\mathrm{max}$")
+plt.title("Resonance curve for decreasing $\omega$")
+plt.plot(omega,xe, lw = 0.5, label = "Euler")
+plt.plot(omega,x2, lw = 0.5, label = "RK2")
+plt.plot(omega,x4, lw = 0.5, label = "RK4")
+
+plt.legend(loc = "upper right")
+plt.savefig("rescurve_dec.pdf",dpi = 400)
+plt.show()
+
